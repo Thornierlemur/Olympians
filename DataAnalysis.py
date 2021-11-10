e6b58197b2cffaa02a3ef04ca32e9890c5db3547
Isaac Valdez <isaac.valdez01@utrgv.edu>
2021-11-09 19:29:50 -0600

    Playing around with data manipulation

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -1,4 +1,12 @@
 import pandas as pd
+import matplotlib as plt
 
 # This is a test comment
+medal = pd.read_csv('medals2.csv')
 
+# print(medal)
+
+# print(medal['country'])
+
+medal['country'].plot()
+plt.show()
