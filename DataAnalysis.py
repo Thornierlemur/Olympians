9eb94446ac95e11437fa96620ea21b9c5b6595c8
Isaac Valdez <isaac.valdez01@utrgv.edu>
2021-11-09 20:04:47 -0600

    matplotlib is now working, added a simple histogram that shows the count of gold medals

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -1,12 +1,17 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 # This is a test comment
-medal = pd.read_csv('medals2.csv')
+medals_df = pd.read_csv('medals2.csv')
+coaches_df = pd.read_csv('coaches2.csv')
+technical_officials_df = pd.read_csv('technical_officials2.csv')
+athletes_df = pd.read_csv('athletes2.csv')
+medals_total_df = pd.read_csv('medals_total2.csv')
 
-# print(medal)
+print(medals_total_df.head(3))
 
-# print(medal['country'])
-
-medal['country'].plot()
+# Plot the amount of gold medals won.
+plt.hist(medals_total_df['Gold Medal'], bins=medals_total_df.shape[0])
+plt.ylabel("Count")
+plt.title("Gold Medals")
 plt.show()
