51b01baaf077272e2dba2a2dc3accfaeb35af8b0
Isaac Valdez <isaac.valdez01@utrgv.edu>
2021-11-11 21:03:28 -0600

    Was able to merge two dataframes on the same column and calculate the ages for the people who won Gold, Silver, and Bronze

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from datetime import date
 
 # This is a test comment
 medals_df = pd.read_csv('medals2.csv')
@@ -9,39 +10,62 @@ technical_officials_df = pd.read_csv('technical_officials2.csv')
 athletes_df = pd.read_csv('athletes2.csv')
 medals_total_df = pd.read_csv('medals_total2.csv')
 
-print(medals_df)
-
 i = 0
 
 old = " "
 list = []
 check = False
 
+# This for loop is getting the data for each of the disciplines!
+# I just need to make it so that i store them whenever I get it!
 for _ in medals_df['discipline']:
-    if old == _:
+    for i in list:
+        if i == _:
+            check = True
+            break
+    if check:
+        check = False
         continue
     else:
-        for i in list:
-            if i == _:
-                check = True
-                break
-        if check:
-            check = False
-            continue
-        else:
-            temparray = np.array(medals_df[medals_df['discipline']==_])
-            print(temparray[0][11])
-            old = _
-            list.append(_)
+        temparray = medals_df[medals_df['discipline']==_]
+        #print(temparray)
+        list.append(_)
 
-    # [access row][access columns] 11='discipline'
+    # [access row][access columns] 11='discipline' (this is when its in the np.array() format)
     # Now, store the discipline into a list of some sort, and then 
-    # convert it back to a pandas dataframe for easy manipulation (so we can just gather certain data and stuff)
+
+myArch = medals_df[medals_df['discipline']=='Archery']
+Archarray = np.array(medals_df[medals_df['discipline']=='Archery'])
 
 
-# for _ in medals_total_df['Country Code']:
-#     temparray = np.array(medals_total_df[medals_total_df['Country Code']==_])
-#     print(temparray[0][1])
+
+# This is a huge testing section where I drop certain columns
+athleteArch = athletes_df[athletes_df['discipline'] == 'Archery']
+athleteArch = athleteArch.drop(columns=['short_name', 'birth_place', 'birth_country', 'country', 'country_code', 'residence_place', "residence_country", "height_m/ft", "url"])
+# Rename the 'name' column to 'athlete_name' for merging purposes
+athleteArch = athleteArch.rename(columns={'name': 'athlete_name'})
+
+result = pd.merge(myArch, athleteArch, how="inner", on='athlete_name')
+
+Gold = result[result['medal_code']==1]
+Silver = result[result['medal_code']==2]
+Bronze = result[result['medal_code']==3]
+
+dob = np.array(Bronze['birth_date'])
+print(dob)
+
+def calculateAge(birthDate):
+    today = date.today()
+    age = today.year - birthDate.year - ((today.month, today.day) <
+         (birthDate.month, birthDate.day))
+ 
+    return age
+
+
+for i in range(len(dob)):
+    line = dob[i].split("-")
+    print(calculateAge(date(int(line[0]), int(line[1]), int(line[2]))), "years")
+    
 
 
 # Plot the amount of gold medals won.
