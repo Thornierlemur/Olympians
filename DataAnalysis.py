7abcf3a488c05bc4f9bc7bd513454c09b99ade91
Isaac Valdez <isaac.valdez01@utrgv.edu>
2021-11-14 15:31:28 -0600

    Made the plots for Gold, Silver, Bronze for all disciplines. Next thing to do is combine all three plots into one big plot for each discipline

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -3,6 +3,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 from datetime import date
 
+def calculateAge(birthDate):
+    today = date.today()
+    age = today.year - birthDate.year - ((today.month, today.day) <
+         (birthDate.month, birthDate.day))
+ 
+    return age
+
 # This is a test comment
 medals_df = pd.read_csv('medals2.csv')
 coaches_df = pd.read_csv('coaches2.csv')
@@ -31,45 +38,53 @@ for _ in medals_df['discipline']:
         #print(temparray)
         list.append(_)
 
-    # [access row][access columns] 11='discipline' (this is when its in the np.array() format)
-    # Now, store the discipline into a list of some sort, and then 
+        # myArch = medals_df[medals_df['discipline']=='Archery']
+        Archarray = np.array(medals_df[medals_df['discipline']==_])
 
-myArch = medals_df[medals_df['discipline']=='Archery']
-Archarray = np.array(medals_df[medals_df['discipline']=='Archery'])
+        # This is a huge testing section where I drop certain columns
+        athleteArch = athletes_df[athletes_df['discipline'] == _]
+        athleteArch = athleteArch.drop(columns=['short_name', 'birth_place', 'birth_country', 'country', 'country_code', 'residence_place', "residence_country", "height_m/ft", "url"])
+        # Rename the 'name' column to 'athlete_name' for merging purposes
+        athleteArch = athleteArch.rename(columns={'name': 'athlete_name'})
 
+        result = pd.merge(temparray, athleteArch, how="inner", on='athlete_name') # temparray before was myArch! (Just a Note to Isaac from himself)
 
+        Gold = result[result['medal_code']==1]
+        Silver = result[result['medal_code']==2]
+        Bronze = result[result['medal_code']==3]
 
-# This is a huge testing section where I drop certain columns
-athleteArch = athletes_df[athletes_df['discipline'] == 'Archery']
-athleteArch = athleteArch.drop(columns=['short_name', 'birth_place', 'birth_country', 'country', 'country_code', 'residence_place', "residence_country", "height_m/ft", "url"])
-# Rename the 'name' column to 'athlete_name' for merging purposes
-athleteArch = athleteArch.rename(columns={'name': 'athlete_name'})
+        MedalList = [Gold, Silver, Bronze]
 
-result = pd.merge(myArch, athleteArch, how="inner", on='athlete_name')
+        count = 1
+        for j in MedalList:
+            dob = np.array(j['birth_date'])
+            print(dob)
 
-Gold = result[result['medal_code']==1]
-Silver = result[result['medal_code']==2]
-Bronze = result[result['medal_code']==3]
+            # Calculate the ages of the people!
+            ages = []
+            for m in range(len(dob)):
+                line = dob[m].split("-")
+                ages.append(calculateAge(date(int(line[0]), int(line[1]), int(line[2]))))
 
-dob = np.array(Bronze['birth_date'])
-print(dob)
+            ages.sort()
+            print(ages)
 
-def calculateAge(birthDate):
-    today = date.today()
-    age = today.year - birthDate.year - ((today.month, today.day) <
-         (birthDate.month, birthDate.day))
- 
-    return age
+            # Gets rid of the duplicates in the array
+            resultA = []
+            for z in ages:
+                if z not in resultA:
+                    resultA.append(z)
+            print(resultA)
 
-
-for i in range(len(dob)):
-    line = dob[i].split("-")
-    print(calculateAge(date(int(line[0]), int(line[1]), int(line[2]))), "years")
-    
-
-
-# Plot the amount of gold medals won.
-# plt.hist(medals_total_df['Gold Medal'], bins=medals_total_df.shape[0])
-# plt.ylabel("Count")
-# plt.title("Gold Medals")
-# plt.show()
+            plt.hist(ages, bins=np.arange(resultA[0] - 0.5, resultA[-1]+1+0.5), edgecolor='white', linewidth=1, align='mid')
+            if count == 1:
+                plt.title("Gold in " + _)
+            elif count == 2:
+                plt.title("Silver in " + _)
+            else:
+                plt.title("Bronze in " + _)
+            plt.xlabel("Ages")
+            plt.ylabel("Count of Medals")
+            plt.xticks(ages)
+            plt.show()
+            count = count + 1
