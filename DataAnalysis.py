8ac307929ab1a2d24a3c3772d76fa5d427654467
Isaac Valdez <isaac.valdez01@utrgv.edu>
2021-11-17 00:07:14 -0600

    Made it so that Gold, Silver, Bronze plots are in one big plot

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -53,38 +53,68 @@ for _ in medals_df['discipline']:
         Silver = result[result['medal_code']==2]
         Bronze = result[result['medal_code']==3]
 
-        MedalList = [Gold, Silver, Bronze]
+        dob = np.array(Gold['birth_date'])
+        dob1 = np.array(Silver['birth_date'])
+        dob2 = np.array(Bronze['birth_date'])
 
-        count = 1
-        for j in MedalList:
-            dob = np.array(j['birth_date'])
-            print(dob)
+        # Calculate the ages of the people!
+        ages = []
+        for m in range(len(dob)):
+            line = dob[m].split("-")
+            ages.append(calculateAge(date(int(line[0]), int(line[1]), int(line[2]))))
+        ages.sort()
 
-            # Calculate the ages of the people!
-            ages = []
-            for m in range(len(dob)):
-                line = dob[m].split("-")
-                ages.append(calculateAge(date(int(line[0]), int(line[1]), int(line[2]))))
+        ages1 = []
+        for m in range(len(dob1)):
+            line = dob1[m].split("-")
+            ages1.append(calculateAge(date(int(line[0]), int(line[1]), int(line[2]))))
+        ages1.sort()
 
-            ages.sort()
-            print(ages)
+        ages2 = []
+        for m in range(len(dob2)):
+            line = dob2[m].split("-")
+            ages2.append(calculateAge(date(int(line[0]), int(line[1]), int(line[2]))))
+        ages2.sort()
 
-            # Gets rid of the duplicates in the array
-            resultA = []
-            for z in ages:
-                if z not in resultA:
-                    resultA.append(z)
-            print(resultA)
+        # Gets rid of the duplicates in the array
+        resultA = []
+        for z in ages:
+            if z not in resultA:
+                resultA.append(z)
+        
+        # Gets rid of the duplicates in the array
+        resultA1 = []
+        for z in ages1:
+            if z not in resultA1:
+                resultA1.append(z)
+        
+        # Gets rid of the duplicates in the array
+        resultA2 = []
+        for z in ages2:
+            if z not in resultA2:
+                resultA2.append(z)
 
-            plt.hist(ages, bins=np.arange(resultA[0] - 0.5, resultA[-1]+1+0.5), edgecolor='white', linewidth=1, align='mid')
-            if count == 1:
-                plt.title("Gold in " + _)
-            elif count == 2:
-                plt.title("Silver in " + _)
-            else:
-                plt.title("Bronze in " + _)
-            plt.xlabel("Ages")
-            plt.ylabel("Count of Medals")
-            plt.xticks(ages)
-            plt.show()
-            count = count + 1
+        # Create 3 plots to plot the data for Gold, Silver, and Bronze
+        fig, axs = plt.subplots(3)
+
+        axs[0].hist(ages, bins=np.arange(resultA[0] - 0.5, resultA[-1]+1+0.5), edgecolor='white', linewidth=1, align='mid')
+        axs[0].title.set_text("Gold in " + _)
+        axs[0].set_xlabel("Ages")
+        axs[0].set_ylabel("Count of Medals")
+        axs[0].set_xticks(ages)
+
+        axs[1].hist(ages1, bins=np.arange(resultA1[0] - 0.5, resultA1[-1]+1+0.5), edgecolor='white', linewidth=1, align='mid')
+        axs[1].title.set_text("Silver in " + _)
+        axs[1].set_xlabel("Ages")
+        axs[1].set_ylabel("Count of Medals")
+        axs[1].set_xticks(ages1)
+
+        axs[2].hist(ages2, bins=np.arange(resultA2[0] - 0.5, resultA2[-1]+1+0.5), edgecolor='white', linewidth=1, align='mid')
+        axs[2].title.set_text("Bronze in " + _)
+        axs[2].set_xlabel("Ages")
+        axs[2].set_ylabel("Count of Medals")
+        axs[2].set_xticks(ages2)
+
+        # plt.xticks(ages)
+        plt.tight_layout(pad=1.0)
+        plt.show()
